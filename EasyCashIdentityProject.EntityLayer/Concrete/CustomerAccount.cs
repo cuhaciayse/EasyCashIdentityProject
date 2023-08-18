d5e09fbfc28b067710f7ad0da108fe0bb8a55564
Ayşe Nur Çuhacı <109754763+cuhaciayse@users.noreply.github.com>
2023-08-18 13:48:48 +0300

    Add project files.

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+        public int CustomerAccountID { get; set; }
+        public string CustomerAccountNumber { get; set; }      /* Müşteri hesap numarası */
+        public string CustomerAccountCurrency { get; set; }    /* Hesabın döviz değeri */
+        public decimal CustomerAccountBalance { get; set; }    /* Hesabın bakiyesi */
+        public string BankBranch { get; set; }                 /* Hesabın bulunduğu banka şubesi */
+    }
+}
---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccountProcess
+    {
+        public int CustomerAccountProcessID { get; set; }
+        public int ProcessType { get; set; }      /* İşlem türü */
+        public decimal Amount { get; set; }       /* Ne kadar para gönderildi */
+        public DateTime ProcessDate { get; set; } /* İşlemin tarihi */
+    }
+}
